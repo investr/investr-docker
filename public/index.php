512439291bf1b8188ad2585993e875825377f2ec
Chirag Rathod <chirag.rathod@gmail.com>
2019-07-23 18:49:39 +0530

    Removed Slim

--- public/index.php
+++ public/index.php
@@ -1,9 +1,14 @@
-<?php require './vendor/autoload.php';
-// instantiate the App object
-$app = new \Slim\App();
-// Add route callbacks
-$app->get('/', function ($request, $response, $args) {
-    return $response->withStatus(200)->write('Hello World!');
-});
-// Run application
-$app->run();
+<?php
+    $dbhost = '172.17.0.2';
+    $dbuser = 'root';
+    $dbpass = 'admin';
+    $dbname = 'investr';
+
+    $mysql_conn_string = "mysql:host=$dbhost;dbname=$dbname";
+    $dbConnection = new PDO($mysql_conn_string, $dbuser, $dbpass);
+
+    $stmt = $pdo->query('SELECT COMMENT FROM DOCKER_TEMP');
+    {
+        echo $row['COMMENT'] . "\n";
+    }
+?>
