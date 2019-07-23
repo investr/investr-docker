09cc8e3d6c38c397dc0cb399889cf8a4f470dc9f
Chirag Rathod <chirag.rathod@gmail.com>
2019-07-23 14:21:01 +0530

    Got Slim working with PHP

--- public/index.php
+++ public/index.php
@@ -1,2 +1,9 @@
-<?php
-echo phpinfo();
+<?php require './vendor/autoload.php';
+// instantiate the App object
+$app = new \Slim\App();
+// Add route callbacks
+$app->get('/', function ($request, $response, $args) {
+    return $response->withStatus(200)->write('Hello World!');
+});
+// Run application
+$app->run();
